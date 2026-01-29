76ee559630c16971c03101a55139bcb25cea0408
Joe <11071329+joembaya@users.noreply.github.com>
2026-01-29 16:07:08 +0100

    Mockae fix

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,14 +1,14 @@
-import { inject, Injectable } from '@angular/core';
-import { Product } from '../models/product.model';
-import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from "@angular/core";
+import { Product } from "../models/product.model";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
-export class ProductsService{
-   private http = inject(HttpClient);
-   getProducts() {
-    return this.http.get<Product[]>('https://api.mockae.com/fakeapi/products');  // CORS ISSUE
-    //return this.http.get<Product[]>('https://jsonplaceholder.typicode.com/posts'); // ok. 
-  }
+export class ProductsService {
+    private http = inject(HttpClient);
+    getProducts() {
+        return this.http.get<Product[]>("/mock-api/fakeapi/products");
+        //return this.http.get<Product[]>('https://jsonplaceholder.typicode.com/posts'); // ok.
+    }
 }
--- src/app/core/stores/products/products.effects.ts
+++ src/app/core/stores/products/products.effects.ts
@@ -1,32 +1,33 @@
-
-import { inject, Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { ProductsService } from '../../services/products.service';
-import * as ProductsActions from './products.actions';
-import { catchError, map, switchMap, of } from 'rxjs';
+import { inject, Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { ProductsService } from "../../services/products.service";
+import * as ProductsActions from "./products.actions";
+import { catchError, map, switchMap, of } from "rxjs";
 
 @Injectable()
 export class ProductsEffects {
     private actions$ = inject(Actions);
     private productsService = inject(ProductsService);
 
-  loadProducts$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(ProductsActions.loadProducts),
-      switchMap(() =>
-        this.productsService.getProducts().pipe(
-          map((products) => {
-            console.log('Products fetched:', products);
-            return ProductsActions.loadProductsSuccess({ products })
-          }
-          ),
-          catchError(error =>
-            of(ProductsActions.loadProductsFailure({ error }))
-          )
-        )
-      )
-    )
-  );
-
+    loadProducts$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(ProductsActions.loadProducts),
+            switchMap(() =>
+                this.productsService.getProducts().pipe(
+                    map((products) => {
+                        console.log("Products fetched:", products);
+                        return ProductsActions.loadProductsSuccess({
+                            products,
+                        });
+                    }),
+                    catchError((error) => {
+                        console.error("HTTP ERROR:", error);
+                        return of(
+                            ProductsActions.loadProductsFailure({ error }),
+                        );
+                    }),
+                ),
+            ),
+        ),
+    );
 }
-
